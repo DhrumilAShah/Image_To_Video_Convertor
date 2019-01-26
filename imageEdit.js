439acad567ec975d6d2257a7edd6422024fe7b0e
DhrumilaShah <dhrumilshah2495@gmail.com>
2019-01-25 19:15:10 -0500

    nodejs

--- imageEdit.js
+++ imageEdit.js
@@ -1,19 +1,19 @@
 var jimp = require('jimp');
 
-var resize = (img,width,height) => {
-console.log(img)
-jimp.read(img)
-  .then(image => {
-    return image
-      .resize(width,height)
-      .quality(100)
-      .write(img);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+var resize = (img, width, height) => {
+  //console.log(img)
+  jimp.read(img)
+    .then(image => {
+      return image
+        .resize(width, height)
+        .quality(100)
+        .write(img);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 }
 //resize('/home/dhrumil/Test/uploads/2.jpg',256,256)
 module.exports = {
-    resize : resize
+  resize: resize
 }
